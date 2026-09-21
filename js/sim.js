c6dafb09cb4880c84c3d0094a1ff14e8eccfff05
motion-mind <motion-mind@users.noreply.github.com>
2026-09-21 22:41:43 +0000

    Relay: fix the CSC-2000 B-route right angle, drop redundant landing stubs, move the M label clear of the B port

--- js/sim.js
+++ js/sim.js
@@ -172,7 +172,7 @@
     var specs = [];
     if (hasRelay) {
       specs.push({ id: "hotAct", points: [[ACTUATOR_X, 237 - actLift], [ACTUATOR_X, 250 - actLift],
-        [RELAY_B[0], 250], [RELAY_B[0], RELAY_B[1]]],
+        [RELAY_B[0], 250 - actLift], [RELAY_B[0], RELAY_B[1]]],
         cls: "wire-act", name: "relay to hot actuator", mode: "ctrl" });
     }
     var tHotY = model === "csc2000" ? 295 : 175;
